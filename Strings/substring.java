37d823d76d7a2c29bd7f9112a56f9e520ec65972
Hrutika Suryawanshi <111946645+hrutikas2002@users.noreply.github.com>
2024-09-19 21:17:42 +0530

    Add files via upload

---
+++ Strings/substring.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class substring {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String str = sc.nextLine();
+
+        System.out.println(str.substring(2, 5));    // Printing substring using string method substring
+    }
+}
